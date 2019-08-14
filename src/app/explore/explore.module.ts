a6e1629896f77ac9f17cdb23a308ed91406e5101
Yossig <yosigartner@gmail.com>
2019-08-14 16:51:04 +0300

    add fab button

--- src/app/explore/explore.module.ts
+++ src/app/explore/explore.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ExploreRoutingModule } from './explore-routing.module';
 import { ExploreComponent } from './explore.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatCardModule, MatGridListModule, MatDividerModule, MatToolbarModule, MatFormFieldModule, MatSelectModule, MatChipsModule, MatIconModule, MatSliderModule } from '@angular/material';
+import { MatCardModule, MatGridListModule, MatDividerModule, MatToolbarModule, MatFormFieldModule, MatSelectModule, MatChipsModule, MatIconModule, MatSliderModule, MatButtonModule } from '@angular/material';
 import { FilterBarComponent } from './filter-bar/filter-bar.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
@@ -25,7 +25,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
     BrowserAnimationsModule,
     MatChipsModule,
     MatIconModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule
   ],
   providers:[],
   bootstrap: [ExploreComponent]
