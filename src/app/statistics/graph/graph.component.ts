c12c5a06133b1178027ebe50d7ff43392c84bd45
Yossig <yosigartner@gmail.com>
2019-08-27 23:24:25 +0300

    done with cms graph

--- src/app/statistics/graph/graph.component.ts
+++ src/app/statistics/graph/graph.component.ts
@@ -9,14 +9,15 @@ import { Statistics } from 'src/app/core/models/statistics.model';
 })
 export class GraphComponent implements OnInit {
   @Input() statisticsData: Statistics
-  width: number = 600;
-  height: number = 250;
+  width: number = 750;
+  height: number = 300;
   barHeight: number = 35;
   barMargin: number = 2;
   margin: any;
   svg: any;
-  xScale;
-  yScale;
+  xScale: any;
+  max: any;
+  min: any;
 
   constructor() {
   }
@@ -24,6 +25,7 @@ export class GraphComponent implements OnInit {
   ngOnInit() {
     this.setup();
     this.buildSvg();
+    this.buildTemplate();
     this.populate();
     this.addText();
 
@@ -34,20 +36,25 @@ export class GraphComponent implements OnInit {
       top: 15,
       right: 15,
       bottom: 15,
-      left: 15
+      left: 85
     }
 
     this.xScale = d3.scaleLinear().range([0, this.width - this.margin.right]).domain([0, this.statisticsData.hitCount])
-
+    this.max = d3.max(this.statisticsData.lastClient, d => d.frequency)
+    this.min = d3.min(this.statisticsData.lastClient, d => d.frequency)
   }
 
   buildSvg() {
     this.svg = d3.select(".chart")
       .attr('width', this.width)
       .attr('height', this.height)
-      .style('background-color','rgb(96,125,139)')
+      .style('background-color', 'rgb(96,125,139)')
       .append('g')
-      .attr('transform', d => 'translate(' + 0 + ',' + (this.height - this.statisticsData.lastClient.length * (this.barHeight + this.barMargin)) / 2 + ')')
+      .attr('transform', d => 'translate(' + 0 + ',' + (this.height - this.statisticsData.lastClient.length * (this.barHeight + this.barMargin)) * 3 / 5 + ')')
+  }
+
+  buildTemplate() {
+    this.svg
       .selectAll('rect')
       .data(this.statisticsData.lastClient)
       .enter()
@@ -56,15 +63,38 @@ export class GraphComponent implements OnInit {
       .attr('y', (d, i) => i * (this.barHeight + this.barMargin))
       .attr('width', d => this.width - this.margin.right - this.margin.left)
       .attr('height', this.barHeight)
-      .attr('fill', 'rgba(255,255,255,0.5)')
-      /*.style('stroke', 'black')
-      .style('stroke-opacity', 0.2)*/
-      .exit()
+      .attr('fill', d => {
+        if (d.frequency === this.max) {
+          return 'rgba(0,255,0,0.5)'
+        } else if (d.frequency === this.min) {
+          return 'rgba(255,0,0,0.5)'
+        } else {
+          return ('rgba(255,255,255,0.5)')
+        }
+      })
+
+    d3.select(".chart")
+      .append('text')
+      .attr('x', (this.width - 248) / 2)
+      .attr('y', 50)
+      .text("Some Stats")
+      .attr('fill', 'white')
+      .style('font-family', "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif")
+      .style('font-size', '3em')
+
+    d3.select(".chart")
+      .append('text')
+      .attr('x', 10)
+      .attr('y', this.height -10)
+      .text("*Data is according to the last client visited the site")
+      .attr('fill', 'white')
+      .style('font-family', "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif")
+      .style('font-size', '0.75em')
 
   }
 
   populate() {
-    this.svg
+    this.svg.selectAll('g')
       .data(this.statisticsData.lastClient)
       .enter()
       .append('rect')
@@ -72,29 +102,39 @@ export class GraphComponent implements OnInit {
       .attr('y', (d, i) => i * (this.barHeight + this.barMargin))
       .attr('width', d => this.xScale(d.frequency) - this.margin.right - this.margin.left)
       .attr('height', this.barHeight)
-      .attr('fill', 'rgba(0,0,0,0.1)')
+      .attr('fill', 'rgba(0,0,0,0.2)')
   }
 
   addText() {
-    this.svg
-    .data(this.statisticsData.lastClient)
-    .enter()
-    .append('text')
-    .attr('x', d => this.margin.left + this.margin.left)
-    .attr('y', (d, i) => i * (this.barHeight + this.barMargin) +  22)
-    .text(d=> d.value)
-    .attr('fill','white')
-
-    this.svg
-    .data(this.statisticsData.lastClient)
-    .enter()
-    .append('text')
-    .attr('x', d =>  this.width - this.margin.right - this.margin.left - 40)
-    .attr('y', (d, i) => i * (this.barHeight + this.barMargin) +  22)
-    .text(d=> (d.frequency*100/this.statisticsData.hitCount).toFixed(1)+'%')
-    .attr('fill','white')
-   
+    this.svg.selectAll('g')
+      .data(this.statisticsData.lastClient)
+      .enter()
+      .append('text')
+      .attr('x', d => this.margin.left + 10)
+      .attr('y', (d, i) => i * (this.barHeight + this.barMargin) + 22)
+      .text(d => d.value)
+      .attr('fill', 'white')
+      .style('font-family', "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif")
+      .exit()
+      .data(this.statisticsData.lastClient)
+      .enter()
+      .append('text')
+      .attr('x', d => this.xScale(d.frequency) - this.margin.right - 45)
+      .attr('y', (d, i) => i * (this.barHeight + this.barMargin) + 22)
+      .text(d => (d.frequency * 100 / this.statisticsData.hitCount).toFixed(1) + '%')
+      .attr('fill', 'white')
+      .style('font-family', "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif")
+      .exit()
+      .data(this.statisticsData.lastClient)
+      .enter()
+      .append('text')
+      .attr('x', d => 10)
+      .attr('y', (d, i) => i * (this.barHeight + this.barMargin) + 22)
+      .text(d => d.field)
+      .attr('fill', 'white')
+      .style('font-family', "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif")
+      .style('font-weight', "bold")
   }
-  
+
 
 }
