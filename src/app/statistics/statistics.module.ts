49357060c1916f2af930c4381a3f81e07e0608f8
Yossig <yosigartner@gmail.com>
2019-08-27 15:51:13 +0300

    rearrange statistics view

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { StatisticsRoutingModule } from './statistics-routing.module';
 import { StatisticsComponent } from './statistics.component';
 import { GraphComponent } from './graph/graph.component';
+import { MatGridListModule, MatIconModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [StatisticsComponent, GraphComponent],
   imports: [
     CommonModule,
-    StatisticsRoutingModule
+    StatisticsRoutingModule,
+    MatGridListModule,
+    MatIconModule
   ]
 })
 export class StatisticsModule { }
