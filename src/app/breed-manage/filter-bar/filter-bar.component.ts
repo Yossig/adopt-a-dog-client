c9325da419927ed588b854c492ce36b34786bff1
Yossig <yosigartner@gmail.com>
2019-08-31 23:28:32 +0300

    add debounce for filter search input

--- src/app/breed-manage/filter-bar/filter-bar.component.ts
+++ src/app/breed-manage/filter-bar/filter-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-filter-bar',
@@ -7,10 +9,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class FilterBarComponent implements OnInit {
   filter: any;
+  filterUpdate = new Subject<any>();
+
   @Input() origins: string[];
   @Output() filterRequest = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {
+    this.filterUpdate.pipe(
+      debounceTime(400),
+      distinctUntilChanged()).subscribe(() => {
+        this.emitFilterRequest()
+      })
+  }
 
   ngOnInit() {
     this.filter = {
